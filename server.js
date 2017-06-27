69eb075e1cecc16d95639d38a74b7235c2be0545
Marshall Munoz <marshmun@gmail.com>
2017-06-27 14:47:27 -0500

    got a user input

--- server.js
+++ server.js
@@ -6,13 +6,78 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-var attempts = []
 
 
+var randomNumber = Math.floor(Math.random() * 100000);
+var randomWord = words[randomNumber];
+var wordLength = randomWord.length;
+var replacedWords = []
+var lettersGuessed = []
 
+for (i = 0; i < wordLength; i += 1) {
+    replacedWords.push("_ ");
+}
+
+
+//template engine
 app.engine("mustache", mustacheExpress());
 app.set('views', './profile');
 app.set("view engine", "mustache")
+
+
+app.use(express.static('./profile'))
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+//routes
+app.listen(port, function () {
+    console.log("Server is running on port", port)
+});
+
+
+app.get('/', function (req, res) {
+    var lettersMatched = ''
+    var lettersGuessed = lettersMatched;
+    var numLettersMatched = 0;
+
+    setup();
+    res.render('index', { words: replacedWords, counter: lives, letters: lettersGuessed });
+});
+
+
+
+app.post('/', function (req, res) {
+    var userGuess = req.body;
+    // see if post is getting the user input
+    console.log(userGuess);
+    //check that user input is valid... one letter, no number
+
+    //check if user input is in random word
+
+    //put user input into random word OR guessed letters
+
+    //display replacedwords and guessed letters
+
+
+    // if (randomWord.indexOf(req) != -1) { // if the character is found
+    //     for (var i = 0; i < wordLength; i++) { // loop on all characters
+    //         if (randomWord[i] == req) // if this is an occurance
+    //             progressWord[i] = chosenWord[i];
+    //     }
+    // } else {
+    //     // wrong choice
+    // }
+
+
+
+    res.redirect('/')
+});
+
+
+
+
+//logic
 
 function setup() {
     /* start config options */
@@ -25,33 +90,3 @@ function setup() {
         validLetter: 'Please enter a letter from A-Z'
     };
 };
-
-var randomNumber = Math.floor(Math.random() * 100000);
-var randomWord = words[randomNumber];
-var wordLength = randomWord.length;
-var replacedWords = []
-
-for (i = 0; i < wordLength; i += 1) {
-    replacedWords.push("_ ");
-}
-
-
-app.get('/', function (req, res) {
-    var lettersMatched = ''
-    var lettersGuessed = lettersMatched;
-    var numLettersMatched = 0;
-    setup();
-    res.render('index', { words: replacedWords, counter: lives });
-});
-
-
-app.use(express.static('./profile'))
-
-app.post('/', function (req, res) {
-
-    res.redirect('/')
-});
-
-app.listen(port, function () {
-    console.log("Server is running on port", port)
-});
